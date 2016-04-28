afb85f8711c5a2ab2b7b9618f5af8fbb0659a59a
SvalovaNastya <Sun-7402@mail.ru>
2016-04-29 03:43:36 +0500

    ошибка

--- SportStore/SportsStore.WebUI/Infrastructure/NinjectControllerFactory.cs
+++ SportStore/SportsStore.WebUI/Infrastructure/NinjectControllerFactory.cs
@@ -6,6 +6,7 @@ using System.Web.Routing;
 using Moq;
 using Ninject;
 using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Concrete;
 using SportsStore.Domain.Entities;
 
 namespace SportsStore.WebUI.Infrastructure
@@ -29,17 +30,18 @@ namespace SportsStore.WebUI.Infrastructure
             // используя его тип
             return controllerType == null ? null : (IController)ninjectKernel.Get(controllerType);
         }
-
+            
         private void AddBindings()
         {
             // конфигурирование контейнера
-            Mock<IProductRepository> mock = new Mock<IProductRepository>();
-            mock.Setup(m => m.Products).Returns(new List<Product> {
-                new Product { Name = "Football", Price = 25 },
-                new Product { Name = "Surf board", Price = 179 },
-                new Product { Name = "Running shoes", Price = 95 }
-            }.AsQueryable());
-            ninjectKernel.Bind<IProductRepository>().ToConstant(mock.Object);
+//            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+//            mock.Setup(m => m.Products).Returns(new List<Product> {
+//             new Product { Name = "Football", Price = 25 },
+//             new Product { Name = "Surf board", Price = 179 },
+//             new Product { Name = "Running shoes", Price = 95 }
+//             }.AsQueryable());
+//            ninjectKernel.Bind<IProductRepository>().ToConstant(mock.Object);
+            ninjectKernel.Bind<IProductRepository>().To<EFProductRepository>();
         }
     }
 }
